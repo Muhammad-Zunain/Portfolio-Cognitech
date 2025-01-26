70dc99c0f4dcdc461d085b673bd36a75762abff1
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2025-01-26 19:58:31 +0500

    cors setting

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173", 
-        "https://cognitech-kappa.vercel.app"
+        "https://cognitechlabs.vercel.app/"
     ],
     credentials: true
 }));
