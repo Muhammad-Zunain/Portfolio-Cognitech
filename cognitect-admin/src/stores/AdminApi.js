0ee7582ef3e2e4b05e8715de32fa134fbd487ede
Muhammad Owais <138030370+MuhammadOwais03@users.noreply.github.com>
2025-03-27 23:29:15 +0500

    Update AdminApi.js

--- cognitect-admin/src/stores/AdminApi.js
+++ cognitect-admin/src/stores/AdminApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:5000/api';
-// const BASE_URL = "https://cognitech-kappa.vercel.app/api";
+// const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = "https://cognitech-kappa.vercel.app/api";
 
 // Get category
 export const getCategories = async () => {
